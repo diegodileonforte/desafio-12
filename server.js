fe46c6b7213569b62c3eb918719dc777b660a66c
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-05-04 19:21:00 -0300

    Websocket 2

--- server.js
+++ server.js
@@ -2,21 +2,43 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const exphbs = require('express-handlebars')
 
 const products = []
+const messages = []
 
 app.use(express.static('public'))
+
+app.engine("hbs", exphbs({
+        extname: ".hbs",
+    })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
 
 io.on('connection', socket => {
     console.log('Un cliente se ha conectado')
     socket.emit('products', products)
+    socket.emit('messages', messages)
 
     socket.on('new-product', data => {
         products.push(data)
         io.sockets.emit('products', products)
     })
-})
 
+    socket.on('new-message', data => {
+        messages.push(data)
+        io.sockets.emit('messages', messages)
+    })
+})
 
 const PORT = process.env.PORT || 8080
 
